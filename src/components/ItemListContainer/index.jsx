28e2734fd51e13ea24194407a31371f012f484a3
talissa <talissasoto20@gmail.com>
2022-11-23 22:42:04 -0300

    contador

--- src/components/ItemListContainer/index.jsx
+++ src/components/ItemListContainer/index.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import ItemCount from "../ItemCount";
 import Title from "../Title";
 
 export const ItemListContainer = () => {
+    const onAdd = (quantity) => {
+        console.log("Compraste ${quantity} unidades");
+    }
 
     return ( 
+        <>
     <Title greeting= "Talissa" />
+    <ItemCount initial={1} stock={6} onAdd={onAdd} />
+    </>
     );
 }
 export default ItemListContainer;
